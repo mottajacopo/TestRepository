1fece43626b463b5453222b562c45fba8a56ec5c
mottajacopo <mottajacopo@gmail.com>
2016-10-05 12:25:05 +0200

    blocco esercizi 1 fatti es 1 e es 2

---
+++ blocco esercizi 1/blocco esercizi 1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace blocco_esercizi_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ blocco esercizi 1/es 02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace es_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort a = 52130;
+            sbyte b = -115;
+            int c = 4825932;
+            byte d = 97;
+            short e = -10000;
+            ushort f = 20000;
+            byte g = 224;
+            long h = 970700000;
+            byte i = 112;
+            sbyte l = -44;
+            int m = -1000000;
+            short n = 1990;
+            long o = 123456789123456789;
+
+        }
+    }
+}
