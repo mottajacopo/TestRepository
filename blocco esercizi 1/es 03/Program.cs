255a29fb4a0f568a24acc2a0c121fa2b9717724f
Claudio Curti <curti.claudio96@gmail.com>
2016-10-05 13:32:16 +0200

    update es3

--- blocco esercizi 1/es 03/Program.cs
+++ blocco esercizi 1/es 03/Program.cs
@@ -6,7 +6,11 @@ namespace es_03
     {
         static void Main(string[] args)
         {
+            string a = "Hello ";
+            string b = "World";
+            object ab = a + b;
 
+            Console.WriteLine("La stringa completa e': {0}", ab);
         }
     }
 }
