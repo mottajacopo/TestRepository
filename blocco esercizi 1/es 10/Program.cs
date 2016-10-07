bd9b9b093e69666a0b52af7fbce030cdcd35f78f
mottajacopo <mottajacopo@gmail.com>
2016-10-07 11:04:29 +0200

    modifica esercizio 10

--- blocco esercizi 1/es 10/Program.cs
+++ blocco esercizi 1/es 10/Program.cs
@@ -6,7 +6,27 @@ namespace es_10
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("inserisci un numero per vedere se e' un numero primo(1<n>100)");
+            double numero = double.Parse(Console.ReadLine());
 
+            if (numero < 0 || numero > 100)
+            {
+                Console.WriteLine("errore : numero non compreso tra 0 e 100");
+                Console.WriteLine("inserisci un numero per vedere se e' un numero primo(1<n>100)");
+            }
+
+            int k= 1;
+            while (k <= Math.Sqrt(numero))
+            {
+                if (numero % k == 0)
+                {
+                    Console.WriteLine("non e' un numero primo");
+                    break;
+                }
+
+                k++;
+            }
+            
         }
     }
 }
