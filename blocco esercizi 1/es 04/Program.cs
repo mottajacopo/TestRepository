b79b03996c30f30f1eb15c7eac58e74659c9c496
mottajacopo <mottajacopo@gmail.com>
2016-10-07 10:43:11 +0200

    fatti esercizi 04, 06, 08.

--- blocco esercizi 1/es 04/Program.cs
+++ blocco esercizi 1/es 04/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace es_04
 {
@@ -10,6 +6,25 @@ namespace es_04
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("inserisci i dati del dipendente");
+
+            Console.WriteLine("nome :");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("cognome :");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("genere (uomo = m , donna = f):");
+            char gender = Char.Parse(Console.ReadLine());
+            Console.WriteLine("numero identificativo :");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("eta' :");
+            byte age = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("la scheda anagrafica e' :");
+            Console.WriteLine("nome : {0} ", firstName);
+            Console.WriteLine("cognome : {0} ", lastName);
+            Console.WriteLine("genere: {0} ", gender);
+            Console.WriteLine("numero identificativo : {0} ", number);
+            Console.WriteLine("eta' : {0} ", age);
         }
     }
 }
--- blocco esercizi 1/es 06/Program.cs
+++ blocco esercizi 1/es 06/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace es_06
 {
@@ -10,6 +7,18 @@ namespace es_06
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("inserisci un numero per sapere se e' pari o dispari");
+            int numero = int.Parse(Console.ReadLine());
+            int resto = numero % 2;
+            if (resto == 0)
+            {
+                Console.WriteLine("{0} e' pari" , numero);
+            }
+            else
+            {
+                Console.WriteLine ("{0} e' dispari" , numero);
+            }
+            
         }
     }
 }
