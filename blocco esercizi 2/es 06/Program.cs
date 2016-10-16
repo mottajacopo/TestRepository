fab3661433baa29cbed9ca3962fb876e87d1c661
Claudio Curti <curti.claudio96@gmail.com>
2016-10-16 22:33:19 +0200

    iniziato es 06

--- blocco esercizi 2/es 06/Program.cs
+++ blocco esercizi 2/es 06/Program.cs
@@ -5,8 +5,35 @@ namespace es_06
 {
     public class Program
     {
+        public static string fromDecToBin (int number) {
+
+            int rest = 0;
+            string binary = "";
+            string digit = "0";
+
+            while (number >= 1)
+            {
+                rest = number % 2;
+                number = number / 2;
+
+                if (rest == 0)
+                {
+                    binary = "0" + binary;
+                }
+                else
+                {
+                    binary = "1" + binary;
+                }   
+            }
+            return binary;
+            }
         static void Main(string[] args)
         {
+            Console.WriteLine("Inserisci un numero intero:");
+            int dec = int.Parse(Console.ReadLine());
+            string bin;
+            bin = fromDecToBin(dec);
+            Console.WriteLine("{0}", bin);
         }
     }
 }
