d2edcec2d750ce2165fe1feac9c2d620e62fdc8d
Claudio Curti <curti.claudio96@gmail.com>
2016-10-16 11:40:14 +0200

    dajeeeee

--- blocco esercizi 1/es 08/Program.cs
+++ blocco esercizi 1/es 08/Program.cs
@@ -8,19 +8,12 @@ namespace es_08
         {
             Console.WriteLine("Inserisci un numero di 4 cifre:");
             string number = Console.ReadLine();
-
-            while (!Char.IsDigit(number[0]) && (!Char.IsDigit(number[1])) && (!Char.IsDigit(number[2])) && (!Char.IsDigit(number[3])))
+ 
+            while(!char.IsDigit(number[0]) || number.Length != 4)   //aggiunto controllo validità del numero
             {
                 Console.WriteLine("Reinserisci un numero di 4 cifre:");
                 number = Console.ReadLine();
-            }
-
-            int controll = int.Parse(number);
-            while (controll > 10000 || controll < 1000)
-            {
-                Console.WriteLine("Reinserisci un numero di 4 cifre:");
-                number = Console.ReadLine();
-                controll = int.Parse(number);
+             
             }
             //somma cifre 
             byte a = (byte)Char.GetNumericValue(number[0]);
