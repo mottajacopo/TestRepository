cc08337a97ccdb93058882effbaf3d2efdcab354
Claudio Curti <curti.claudio96@gmail.com>
2016-10-18 08:08:45 +0200

    test unit 06

---
+++ blocco esercizi 2/UnitTestBlocco2/es06Test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using es_06;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestBlocco2
+{
+    [TestClass]
+    public class Es06Test
+    {
+        [TestMethod]
+        public void TestEs06FromDecToBin()
+        {
+            string result;
+            result = Program.fromDecToBin("13");
+            Assert.AreEqual(result, "1101");
+
+            result = Program.fromDecToBin("0");
+            Assert.AreEqual(result, "0");
+
+            result = Program.fromDecToBin("23c4f");
+            Assert.AreEqual(result, "Impossibile! Il numero inserito non è decimale.");
+
+        }
+
+        [TestMethod]
+        public void TestEs06FromBinToDec()
+        {
+            string result;
+            result = Program.fromBinToDec("1110101");
+            Assert.AreEqual(result, "117");
+
+            result = Program.fromBinToDec("452");
+            Assert.AreEqual(result, "Impossibile! Il numero inserito non è binario.");
+
+        }
+    }
+}
--- blocco esercizi 2/es 06/Program.cs
+++ blocco esercizi 2/es 06/Program.cs
@@ -5,35 +5,98 @@ namespace es_06
 {
     public class Program
     {
-        public static string fromDecToBin (int number) {
+        public static void isInteger(ref string input)         //controllo per verificare che vengano inseriti numeri e non lettere o altro
+        {
+            int i = 0;
+            bool testDigit = false;
 
-            int rest = 0;
-            string binary = "";
-            string digit = "0";
-
-            while (number >= 1)
+            while (testDigit == false)
             {
-                rest = number % 2;
-                number = number / 2;
+                testDigit = true;
+
+                for (; i < input.Length; i++)
+                {
+                    if (!Char.IsDigit(input[i]) || (input[i] == '-'))         //se il char non è un numero lo reinserisco
+                    {
+                        if (input[i] != 'a' || input[i] != 'b' || input[i] != 'c' || input[i] != 'd' || input[i] != 'e' || input[i] != 'f')             //accetto solo i caratteri "a", "b", "c", "d", "e", "f".
+                        {
+                            Console.WriteLine("Errore! Inserisci di nuovo un numero:");
+                            input = Console.ReadLine();
+                            testDigit = false;
+                            i = 0;
+                        }
+                    }
+                }
+            }
+            return;
+        }
+        public static string fromDecToBin (string numString) {
+
+            string DecToBin = "";
+            for (int i = 0; i < numString.Length; i++) //se il numero inserito è esadecimale questa conversione non è possibile
+            {
+                if ((numString[i] == 'a') || (numString[i] == 'b') || (numString[i] == 'c') || (numString[i] == 'd') || (numString[i] == 'e') || (numString[i] == 'f'))
+                {
+                    DecToBin = "Impossibile! Il numero inserito non è decimale.";
+                    return DecToBin;
+                }
+            }
+            int numInt = int.Parse(numString);
+            int rest = 0;
+
+            if (numInt == 0) //caso particolare: il numero è 0
+            {
+                return "0";
+            }
+            while (numInt >= 1)
+            {
+                rest = numInt % 2;
+                numInt = numInt / 2;
 
                 if (rest == 0)
                 {
-                    binary = "0" + binary;
+                    DecToBin = "0" + DecToBin;
                 }
                 else
                 {
-                    binary = "1" + binary;
+                    DecToBin = "1" + DecToBin;
                 }   
             }
-            return binary;
+            return DecToBin;
             }
+        public static string fromBinToDec (string numString)
+        {
+            double BinToDec = 0; //contiene il numero trasformato in decimale, verrà convertita in stringa alla fine
+            for (int i = 0; i < numString.Length; i++)
+            {
+                if ((numString[i] != '0') && (numString[i] != '1'))
+                {
+                    numString = "Impossibile! Il numero inserito non è binario.";
+                    return numString;
+                }
+            }
+         
+            for (int k = 0; k < numString.Length; k++)
+            {
+                if (numString[k] == '1')
+                {
+                    BinToDec += (Math.Pow(2, numString.Length - k - 1));
+                }
+            }
+            numString = Convert.ToString(BinToDec);
+            return numString;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Inserisci un numero intero:");
-            int dec = int.Parse(Console.ReadLine());
-            string bin;
-            bin = fromDecToBin(dec);
-            Console.WriteLine("{0}", bin);
+            string numString;
+
+            Console.WriteLine("Inserisci un numero intero per ottenere le possibili conversioni:");
+            numString = Console.ReadLine();
+            isInteger(ref numString); //controllo che sia un numero binario, decimale o al massimo esadecimale
+     
+            Console.WriteLine("Da decimale a binario: {0}", fromDecToBin(numString));
+            Console.WriteLine("Da binario a decimale: {0}", fromBinToDec(numString));
+
         }
     }
 }
