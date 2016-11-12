513414db459b8cbf121c267c79ac44b3fe53b89e
Claudio Curti <curti.claudio96@gmail.com>
2016-11-12 16:50:44 +0100

    Aggiunti test del 2 e dell'8

---
+++ blocco esercizi 3/UnitTestBlocco3/es02Test.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using es_02;
+
+namespace UnitTestBlocco3
+{
+    [TestClass]
+    public class Es02Test
+    {
+        [TestMethod]
+        public void TestSumSimple()
+        {
+            string num1 = "2459872";
+            string num2 = "190";
+            string result = Program.Sum(num1, num2);
+            Assert.AreEqual("2460062", result);
+        }
+
+        [TestMethod]
+        public void TestSumSimple2()
+        {
+            string num1 = "9";
+            string num2 = "101";
+            string result = Program.Sum(num1, num2);
+            Assert.AreEqual("110", result);
+        }
+        [TestMethod]
+        public void TestSumComplex()
+        {
+            string num1 = "294827482019257385294827482019257385294827482019257385294827482019257385294827482019257385";
+            string num2 = "12";
+            string result = Program.Sum(num1, num2);
+            Assert.AreEqual("294827482019257385294827482019257385294827482019257385294827482019257385294827482019257397", result);
+        }
+        [TestMethod]
+        public void TestSumCarry()
+        {
+            string num1 = "9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999";
+            string num2 = "1";
+            string result = "10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+            Assert.AreEqual(result, Program.Sum(num1, num2));
+        }
+    }
+}
--- blocco esercizi 3/UnitTestBlocco3/es08Test.cs
+++ blocco esercizi 3/UnitTestBlocco3/es08Test.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using es_08;
 using System.Text;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using es_08;
 
 namespace UnitTestBlocco3
 {
@@ -11,10 +12,20 @@ namespace UnitTestBlocco3
         [TestMethod]
         public void TestEncode()
         {
-            string input = "Test";
-            string cipher = "ab";
-            string encoded = Program.Encode(input, cipher);
-            string resultExpected = "\\u0035\\u0007\\u0012\\u0016";
+            string message = "Questa è una prova";
+            string cipher = "test";
+            string encoded = Program.Encode(message, cipher);
+            string resultExpected = "\\u0025\\u0010\\u0016\\u0007\\u0000\\u0004\\u0053\\u009c\\u0054\\u0010\\u001d\\u0015\\u0054\\u0015\\u0001\\u001b\\u0002\\u0004";
+            Assert.AreEqual(resultExpected, encoded);
+        }
+
+        [TestMethod]
+        public void TestEncode2()
+        {
+            string message = "Messaggio da criptare";
+            string cipher = "codice";
+            string encoded = Program.Encode(message, cipher);
+            string resultExpected = "\\u002e\\u000a\\u0017\\u001a\\u0002\\u0002\\u0004\\u0006\\u000b\\u0049\\u0007\\u0004\\u0043\\u000c\\u0016\\u0000\\u0013\\u0011\\u0002\\u001d\\u0001";
             Assert.AreEqual(resultExpected, encoded);
         }
     }
