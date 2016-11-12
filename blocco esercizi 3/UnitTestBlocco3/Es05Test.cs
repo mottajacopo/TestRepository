2b78f9569b075eda7d72c0e14083e958087ddfff
mottajacopo <mottajacopo@gmail.com>
2016-11-12 10:49:02 +0100

    aggiunto test es 05

---
+++ blocco esercizi 3/UnitTestBlocco3/Es05Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using es_05;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestBlocco3
+{
+    [TestClass]
+    public class es05Test
+    {
+        [TestMethod]
+        public void TestRandomAdvertisement()
+        {
+            string PhraseTest1 = Program.RandomAdvertisement();
+            string PhraseTest2 = Program.RandomAdvertisement();
+            string PhraseTest3 = Program.RandomAdvertisement();
+
+            Assert.AreNotSame(PhraseTest1, PhraseTest2, PhraseTest3);
+        }
+
+    }
+}
