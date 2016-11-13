a85d4a95f44fa03d58fd48cef89f070e2940399a
mottajacopo <mottajacopo@gmail.com>
2016-11-13 17:18:42 +0100

    es07 + test

--- blocco esercizi 3/es 07/Program.cs
+++ blocco esercizi 3/es 07/Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
-using System.ComponentModel;
+using System.Text;
 using System.Net;
-using static System.Net.Mime.MediaTypeNames;
+using System.IO;
 
 namespace es_07
 {
-    class Program
+    public static class Program
     {
-      
+        public static void Download(string url, string filename)
+        {
+            WebClient webClient = new WebClient();
+
+            try
+            {
+                webClient = new WebClient();
+                webClient.DownloadFile(url, filename);
+                Console.WriteLine("download eseguito con successo .");
+            }
+            catch (WebException)
+            {
+                Console.WriteLine("Errore . Si è verificata un eccezione di tipo WeException .");
+                Console.WriteLine("L'URI formato dalla combinazione BaseAddress e address non è valido.");
+                Console.WriteLine(" - oppure -");
+                Console.WriteLine(" filename è null o Empty.");
+                Console.WriteLine(" - oppure -");
+                Console.WriteLine("  Il file specificato non esiste.");
+                Console.WriteLine(" - oppure - si è verificato un errore durante il download dei dati.");
+            }
+            catch (NotSupportedException)
+            {
+                Console.WriteLine("Errore . Si è verificata un eccezione di tipo NotSupportedException .");
+                Console.WriteLine("Il metodo è stato chiamato simultaneamente su più thread.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Errore . Si è verificata un eccezione di tipo Exception .");
+                Console.WriteLine("	Controllare che il valore del parametro url non sia null.");
+            }
+            finally
+            {
+                webClient.Dispose();
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci l indirizzo url del file da scaricare :");
+            string url = Console.ReadLine();
+            Console.WriteLine("rimominare il file :");
+            string fileName = Console.ReadLine();
+
+            Download(url, fileName);
+            
+        }
+
     }
+
+
 }
