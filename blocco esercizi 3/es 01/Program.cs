792159ba56eec6964e62893f26e6b9265770db12
Claudio Curti <curti.claudio96@gmail.com>
2016-11-14 22:07:58 +0100

    uno

--- blocco esercizi 3/es 01/Program.cs
+++ blocco esercizi 3/es 01/Program.cs
@@ -52,19 +52,47 @@ namespace es_01
                 array[i] = IsInteger(ref input);
             }
 
-            Console.WriteLine("inserisci la posizione dell elemento del quale vuoi verificare se è maggiore dei 2 elementi vicini");
+            Console.WriteLine("Inserisci la posizione dell' elemento del quale vuoi verificare se è maggiore dei 2 elementi vicini:");
             input = Console.ReadLine();
             position = IsInteger(ref input);
 
-            result = GreaterThanNeighbours(array, n , position);
+            switch (position) {
+                case 0:
+                    if (array[0] > array[1])
+                    {
+                        Console.WriteLine("Il primo elemento è maggiore del suo vicino, che è {0}", array[1]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Il primo elemento non è maggiore del suo vicino, che è {0}.", array[1]);
+                    }
+                    break;
+             
+                default:
+                    if (position == (n - 1))
+                    {
+                        if (array[array.Length - 1] > array[array.Length - 2])
+                        {
+                            Console.WriteLine("L'ultimo elemento è maggiore del suo vicino, che è {0}", array[array.Length - 2]);
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("L'ultimo elemento non è maggiore del suo vicino, che è {0}", array[array.Length - 2]);
+                            break;
+                        }
+                    }
+                    result = GreaterThanNeighbours(array, n, position);
 
-            if (result == true)
-            {
-                Console.WriteLine("l elemento {0} è maggiore di entrambi i suoi vicini che sono {1} e {2}", array[position], array[position - 1], array[position + 1]);
-            }
-            else
-            {
-                Console.WriteLine("l elemento {0} non è maggiore di entrambi i suoi vicini che sono {1} e {2}", array[position], array[position - 1], array[position + 1]);
+                    if (result == true)
+                    {
+                        Console.WriteLine("L'elemento {0} è maggiore di entrambi i suoi vicini che sono {1} e {2}", array[position], array[position - 1], array[position + 1]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("L' elemento {0} non è maggiore di entrambi i suoi vicini che sono {1} e {2}", array[position], array[position - 1], array[position + 1]);
+                    }
+                    break;
             }
         }
     }
