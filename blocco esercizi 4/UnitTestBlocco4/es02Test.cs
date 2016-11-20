97f653533984e8d474c46509a768c65a02eefe9c
Claudio Curti <curti.claudio96@gmail.com>
2016-11-20 22:58:57 +0100

    es02 + test che non si vede

--- blocco esercizi 4/UnitTestBlocco4/es02Test.cs
+++ blocco esercizi 4/UnitTestBlocco4/es02Test.cs
@@ -2,8 +2,76 @@
 using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using es_02;
 
 namespace UnitTestBlocco4
 {
-  
+    public class TestLibrary
+    {
+        private Library libraryForTest = null;
+
+        public void InizializationOfBooks()
+        {
+            libraryForTest = new Library("Libreria per il test");
+            libraryForTest.AddBook(new Book("It", "Stephen King", "Viking", "Settembre 1986", "0-670-81302-8"));
+            libraryForTest.AddBook(new Book("Il miglio verde", "Stephen King", "Signet Books", "Settembre 1996", "978-0451190499"));
+            libraryForTest.AddBook(new Book("Cujo", "Stephen King", "Viking", "Settembre 1982", "978-0-670-45193-7"));
+            libraryForTest.AddBook(new Book("Robinson Crusoe", "Daniel Defoe", " Bickers and Bush", "Aprile 1719", "978-53-22-027763-8"));
+            libraryForTest.AddBook(new Book("Il signore degli anelli", "J.R.R. Tolkien", "Bompiani", "Ottobre 2000", "978-88-452-9005-0"));
+        }
+
+        [TestMethod]
+        public void TestSearchAuthor()
+        {
+            List<Book> foundBooks = libraryForTest.SearchAuthor("Stephen King");
+            Assert.AreEqual(3, foundBooks.Count); //controllo numero di libri trovati
+            Assert.AreEqual("It", foundBooks[0].Title);
+        }
+        [TestMethod]
+        public void DisplayInformationOfBook()
+        {
+            string[] parameters = libraryForTest.DisplayInformation("Il signore degli anelli");
+            Assert.AreEqual("Il signore degli anelli", parameters[0]);
+            Assert.AreEqual("J.R.R. Tolkien", parameters[1]);
+            Assert.AreEqual("Bompiani", parameters[2]);
+            Assert.AreEqual("Ottobre 2000", parameters[3]);
+            Assert.AreEqual("978-88-452-9005-0", parameters[4]);
+        }
+        [TestMethod]
+        public void TestSearchTitleFounded()
+        {
+            Book searchedTitle = libraryForTest.SearchTitle("Il miglio verde");
+            Assert.AreEqual("Il miglio verde", searchedTitle.Title);
+        }
+        [TestMethod]
+        public void TestSearchTitleNotFounded()
+        {
+            Book searchedTitle = libraryForTest.SearchTitle("Il milio verde");
+            Assert.AreEqual(null, searchedTitle);
+        }
+        
+        [TestMethod]
+        public void DeleteBooksOfAuthors()
+        {
+            bool delete = libraryForTest.DeleteBooksOfAuthor("Stephen King");
+            Assert.AreEqual(true, delete);
+            Book searchedBookDeleted1 = libraryForTest.SearchTitle("Cujo");
+            Assert.AreEqual(null, searchedBookDeleted1);
+            Book searchedBookDeleted2 = libraryForTest.SearchTitle("Il miglio verde");
+            Assert.AreEqual(null, searchedBookDeleted2);
+            Book searchedBookDeleted3 = libraryForTest.SearchTitle("It");
+            Assert.AreEqual(null, searchedBookDeleted3);
+        }
+
+        [TestMethod]
+        public void DeleteSingleBook()
+        {
+            bool delete = libraryForTest.DeleteBooksOfAuthor("Robinson Crusoe");
+            Assert.AreEqual(true, delete);
+            Book searchedBookDeleted = libraryForTest.SearchTitle("Robinson Crusoe");
+            Assert.AreEqual(null, searchedBookDeleted);
+        }
+
+    }
 }
+
---
+++ blocco esercizi 4/es 02/Library.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es_02
+{
+    public class Library
+    {
+        private string nameLibrary = null;
+        private List<Book> bookList;
+
+        public Library(string nameLibrary) //costruttore
+        {
+            this.nameLibrary = nameLibrary;
+            this.bookList = new List<Book>();
+        }
+
+        public string NameLibrary
+        {
+            get
+            {
+                return this.nameLibrary;
+            }
+            set
+            {
+                this.nameLibrary = value;
+            }
+        }
+        public List<Book> BookList
+        {
+            get
+            {
+                return this.bookList;
+            }
+            set
+            {
+                this.bookList = value;
+            }
+        }
+
+        public bool AddBook(Book newBook) //metodo per aggiungere un libro
+        {
+            BookList.Add(newBook);
+            return true;
+        }
+        public List<Book> SearchAuthor(string authorName) //metodo per trovare i libri di un autore
+        {
+            List<Book> bookAuthor = null; ; //lista libri di un autore
+            foreach (Book book in bookList)
+            {
+                if (book.Author.Equals(authorName))
+                {
+                    bookAuthor.Add(book);
+                }
+            }
+            return bookAuthor;
+        }
+        public Book SearchTitle(string bookTitle) 
+        {
+            foreach(Book book in bookList)
+            {
+                if(book.Title.Equals(bookTitle))
+                {
+                    return book;
+                }
+            }
+            return null;
+        }
+        public string[] DisplayInformation (string bookTitle) //parametri del libro
+        {
+            Book bookTitleFounded = SearchTitle(bookTitle);
+            string[] parameters = new string[5];
+
+            parameters[0] = bookTitleFounded.Title;
+            parameters[1] = bookTitleFounded.Author;
+            parameters[2] = bookTitleFounded.Publisher;
+            parameters[3] = bookTitleFounded.ReleaseDate;
+            parameters[4] = bookTitleFounded.Isbn;
+
+            return parameters;
+        }
+        public bool DeleteBooksOfAuthor(string authorName)
+        {
+            foreach (Book book in bookList)
+            {
+                if (book.Author.Equals(authorName))
+                {
+                    BookList.Remove(book);
+                }
+            }
+            return true;
+        }
+        public bool DeleteSingleBook(string bookTitle)
+        {
+            foreach (Book book in bookList)
+            {
+                if (book.Title.Equals(bookTitle))
+                {
+                    BookList.Remove(book);
+                }
+            }
+            return true;
+        }
+
+    }
+}
