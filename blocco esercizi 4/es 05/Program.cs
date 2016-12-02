5dfd68d1c7d8f4cccaf8c14707ae629c94a5bdb5
mottajacopo <mottajacopo@gmail.com>
2016-12-02 11:36:36 +0100

    es03+test , es05

--- blocco esercizi 4/es 05/Program.cs
+++ blocco esercizi 4/es 05/Program.cs
@@ -10,6 +10,35 @@ namespace es_05
     {
         static void Main(string[] args)
         {
+            Graph g = new Graph(new int[][] {
+            new int[] {1,2},
+            new int[] {0},
+            new int[] {3},
+            new int[] {1},
+            new int[] {5},
+            new int[] {3},
+            new int[] {6,7},
+            new int[] {8},
+            new int[] {6}});
+
+            Console.Write("Depth-First Search (DFS) traversal (recursive): ");
+            bool[] visited = new bool[Graph.MaxNode];
+            int counter = 0;
+           
+
+            Console.WriteLine();
+            for (int i = 0; i < g.NumberOfNode; i++)
+            {
+                if (visited[i] == false)
+                {
+                    g.TraverseDFSRecursive(i, visited );
+                    counter++;
+                    Console.WriteLine("sottografo {0}", counter);
+                   
+                }
+            }
+            
         }
+
     }
 }
