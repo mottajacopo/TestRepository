993ec18297524debdef367d708552c390ca40747
Claudio Curti <curti.claudio96@gmail.com>
2016-12-13 11:30:14 +0100

    test

---
+++ blocco esercizi 5/UnitTestBlocco5/es09Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestBlocco5
+{
+    [TestClass]
+    public class es09Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
